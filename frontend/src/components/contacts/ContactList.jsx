b6bf2863f456502b1cdf14549f532c999876e156
Elvan H <elvanhd@gmail.com>
2022-03-20 20:48:18 +0700

    basic list animation

--- frontend/src/components/contacts/ContactList.jsx
+++ frontend/src/components/contacts/ContactList.jsx
@@ -1,27 +1,29 @@
 import { useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { ContactContext } from '../../context/contact/ContactState';
 import ContactItem from './ContactItem';
 
 const ContactList = () => {
   const { contacts, filtered } = useContext(ContactContext);
 
-  const filteredContacts = filtered ?? contacts;
+  const filteredContacts =
+    filtered && filtered.length > 0 ? filtered : contacts;
 
-  if (contacts.length === 0) {
+  if (filteredContacts.length === 0) {
     return <h4>No contacts found</h4>;
-  }
-
-  if (filtered?.length === 0) {
-    return <h4>No filtered contacts found</h4>;
   }
 
   return (
     <>
-      {filteredContacts &&
-        filteredContacts.length > 0 &&
-        filteredContacts.map((contact) => (
-          <ContactItem key={contact.id} contact={contact} />
-        ))}
+      <TransitionGroup>
+        {filteredContacts &&
+          filteredContacts.length > 0 &&
+          filteredContacts.map((contact) => (
+            <CSSTransition key={contact.id} timeout={500} classNames='item'>
+              <ContactItem contact={contact} />
+            </CSSTransition>
+          ))}
+      </TransitionGroup>
     </>
   );
 };
--- frontend/src/context/contact/contactReducer.js
+++ frontend/src/context/contact/contactReducer.js
@@ -21,7 +21,7 @@ const contactReducer = (state, action) => {
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload],
+        contacts: [action.payload, ...state.contacts],
       };
     case UPDATE_CONTACT:
       return {
