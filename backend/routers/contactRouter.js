99feeb01a01abab98c2807b3fc2b4b0e6b10c524
Elvan H <elvanhd@gmail.com>
2022-03-20 15:10:51 +0700

    update and delete contacts

--- backend/routers/contactRouter.js
+++ backend/routers/contactRouter.js
@@ -6,10 +6,10 @@ const Contact = require('../models/contact');
 const router = express.Router();
 
 router.get('/', authGuard, async (req, res) => {
-  // @ts-ignore
-  const userId = req.user._id;
-
   try {
+    // @ts-ignore
+    const userId = req.user._id;
+
     const contacts = await Contact.find({
       user: userId,
     });
@@ -32,12 +32,12 @@ router.get('/', authGuard, async (req, res) => {
 });
 
 router.get('/:id', authGuard, async (req, res) => {
-  // @ts-ignore
-  const userId = req.user._id;
-
-  const { id } = req.params;
-
   try {
+    // @ts-ignore
+    const userId = req.user._id;
+
+    const { id } = req.params;
+
     const contact = await Contact.findById(id);
 
     if (!contact) {
@@ -46,8 +46,10 @@ router.get('/:id', authGuard, async (req, res) => {
     }
 
     if (contact.user.toString() !== userId.toString()) {
-      console.log('Contact not found');
-      return res.status(404).json({ message: 'Contact not found' });
+      console.log('Not authorized to access this contact');
+      return res
+        .status(401)
+        .json({ message: 'Not authorized to access this contact' });
     }
 
     res.json({
@@ -63,12 +65,12 @@ router.get('/:id', authGuard, async (req, res) => {
 });
 
 router.post('/', authGuard, async (req, res) => {
-  // @ts-ignore
-  const userId = req.user._id;
-
-  const { name, email, phone, type } = req.body;
-
   try {
+    // @ts-ignore
+    const userId = req.user._id;
+
+    const { name, email, phone, type } = req.body;
+
     const newContact = new Contact({
       user: userId,
       name,
@@ -88,25 +90,75 @@ router.post('/', authGuard, async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  const { name, email, phone } = req.body;
+router.put('/:id', authGuard, async (req, res) => {
+  try {
+    // @ts-ignore
+    const userId = req.user._id;
 
-  res.json({
-    message: 'Contact updated successfully',
-    contact: {
-      name,
-      email,
-      phone,
-    },
-  });
+    const { name, email, phone, type } = req.body;
+
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      console.log('Contact not found');
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
+    if (contact.user.toString() !== userId.toString()) {
+      console.log('Not authorized to access this contact');
+      return res
+        .status(401)
+        .json({ message: 'Not authorized to access this contact' });
+    }
+
+    contact.name = name;
+    contact.email = email;
+    contact.phone = phone;
+    contact.type = type;
+
+    const updatedContact = await contact.save();
+
+    res.status(200).json({
+      message: 'Contact updated successfully',
+      contact: updatedContact,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error updating contact' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
+router.delete('/:id', authGuard, async (req, res) => {
+  try {
+    // @ts-ignore
+    const userId = req.user._id;
 
-  res.json({
-    message: 'Contact deleted successfully',
-  });
+    const id = req.params.id;
+
+    const contact = await Contact.findById(req.params.id);
+
+    if (!contact) {
+      console.log('Contact not found');
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
+    if (contact.user.toString() !== userId.toString()) {
+      console.log('Not authorized to access this contact');
+      return res
+        .status(401)
+        .json({ message: 'Not authorized to access this contact' });
+    }
+
+    await contact.remove();
+
+    res.json({
+      message: 'Contact deleted successfully',
+      id: id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error deleting contact' });
+  }
 });
 
 module.exports = router;
